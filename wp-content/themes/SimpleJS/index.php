d9b9dda84ba7d8274e8688df771df59d96604baf
Rashmi Malpande <rashmimalpande@gmail.com>
2017-03-20 18:20:23 +0530

    change single post layout & add some styling

--- wp-content/themes/SimpleJS/index.php
+++ wp-content/themes/SimpleJS/index.php
@@ -1,59 +1,63 @@
 <?php get_header(); ?>
 
     <div class="wrapper">
-    
         <div id="app">
+            
             <router-view></router-view>
         </div>
     </div>    
 
     <template id="post-list-template">
-        <div class="container">
-            <div class="post-list">
-                <article v-for="post in posts">
-                    <h2 class="post-title"><router-link :to="{name:'post', params:{slug: post.slug}}">{{ post.title.rendered }}</router-link></h2>
-                    <p v-html="post.excerpt.rendered"></p>
-                </article>
-            </div>
+        <div>
+            <header class="home-banner"></header>
+            <div class="container">
+                <div class="post-list">
+                    <article v-for="post in posts">
+                        <h2 class="post-title"><router-link :to="{name:'post', params:{slug: post.slug}}">{{ post.title.rendered }}</router-link></h2>
+                        <p v-html="post.excerpt.rendered"></p>
+                    </article>
+                </div>
 
-            <div class="pagination">
-                <span>Page {{currentPage}} of {{allPages}}</span>
-                <button class="btn" v-on:click="fetchData(prev_page)" :disabled="!prev_page">
-                    Previous
-                </button>
-                
-                <button class="btn" v-on:click="fetchData(next_page)" :disabled="!next_page">
-                    Next
-                </button>
+                <div class="pagination">
+                    
+                    <button class="btn" v-on:click="fetchData(prev_page)" :disabled="!prev_page">
+                        Previous
+                    </button>
+                    <span>Page {{currentPage}} of {{allPages}}</span>
+                    <button class="btn" v-on:click="fetchData(next_page)" :disabled="!next_page">
+                        Next
+                    </button>
+                </div>
             </div>
         </div>
     </template>
 
 
     <template id="single-post-template">
-        
-        <div class="container">
-            <div class="post-list" v-for="single_post in post">
-                <div class="banner">
+        <div class="post-wrapper">
+            <main v-for="single_post in post">
+                 <div class="banner">
                     <img :src="single_post.featured_url">
                 </div>
-                <h2 class="post-title">{{single_post.title.rendered}}</h2> 
-            
-               <div class="post-content" v-html="single_post.content.rendered"></div>
-               <div class="category">
-                    <span v-for="category in single_post.category_names
-                    ">
-                        <router-link :to="{name:'category', params:{catId: category.cat_ID}}">{{category.name}}</router-link>
-                    </span>
-               </div>
+                <header>
+                    <h1>{{single_post.title.rendered}}</h1>
+                    <p>by {{single_post.author_name}}</p>
+                </header>
+                <article>
+                    <div class="post-content" v-html="single_post.content.rendered"></div>
+                    <div class="category">
+                        <span v-for="category in single_post.category_names">
+                            <router-link :to="{name:'category', params:{catId: category.cat_ID}}">{{category.name}}</router-link>
+                        </span>
+                    </div>
 
-               <div class="next-prev">
-                    <p v-if="single_post.prev_post"><router-link :to="{name:'post', params:{slug: single_post.prev_post}}">Prev Post</router-link></p>
-                    <p v-if="single_post.next_post"><router-link :to="{name:'post', params:{slug: single_post.next_post}}">Next Post</router-link></p>
-               </div>
-            </div>
+                    <div class="next-prev">
+                        <p v-if="single_post.prev_post"><router-link :to="{name:'post', params:{slug: single_post.prev_post}}">Prev Post</router-link></p>
+                        <p v-if="single_post.next_post"><router-link :to="{name:'post', params:{slug: single_post.next_post}}">Next Post</router-link></p>
+                    </div>
+                </article>
+            </main>
         </div>
-
     </template>
 
 
@@ -66,6 +70,8 @@
                     <p v-html="post.excerpt.rendered"></p>
                 </article>
             </div>
+
+        
         </div>
     </template>
 
